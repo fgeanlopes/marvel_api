1c9e83a8df8efc0f1df580f82bd92faf3d220c11
fgeanlopes <fgeanlopes@gmail.com>
2020-08-06 03:07:58 -0300

    adicionado a funcao de visualizao ao click

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ListApi from "./ListApi";
+
 import './App.css';
+const logoMarvel = require("./dist/img/MarvelLogo.svg");
 
 function App() {
   return (
     <div className="App">
+      <div className="header">
+        <img className="logoMarvel" src={logoMarvel} title="logo da Marvel" alt="logo da Marvel" />
+      </div>
       <ListApi />
     </div >
   );
--- src/ListApi.js
+++ src/ListApi.js
@@ -1,4 +1,4 @@
-import React, { createElement, Component } from "react";
+import React, { Component } from "react";
 import api from "./Api";
 import md5 from "md5";
 
@@ -9,27 +9,87 @@ const privateKey = "8d2a7ca5d3ea634c5276a2e1cef0575374c1f5da";
 const publicKey = "c3c8876fa5fa3d1baa02d617d24157c2";
 const md5Final = md5(timeInMs + privateKey + publicKey);
 
-const listComics = baseUrl + "comics?ts=" + timeInMs + "&apikey=" + publicKey + "&hash=" + md5Final;
+const listComics = baseUrl + "comics?limit=100&ts=" + timeInMs + "&apikey=" + publicKey + "&hash=" + md5Final;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class ListApi extends Component {
-    state = {
-        comics: []
+    constructor(props) {
+        super(props);
+
+        this.openComic = this.openComic.bind(this);
+        this.state = {
+            comics: [],
+            nome: "",
+            descricao: "",
+            img: "",
+            autores: "",
+        }
     }
+
     async componentDidMount() {
         const response = await api.get(listComics);
-        console.log(response.data.data.results);
+        // console.log(response.data.data.results);
         this.setState({ comics: response.data.data.results })
     }
+
+    openComic = (id) => {
+        const { comics } = this.state;
+
+        const result = comics.filter(function (element) {
+            // console.log(element.id);
+            if (element.id == id) {
+                console.log(element);
+                return element
+            } else {
+                return false;
+            }
+        });
+
+        const viewClick = result[0];
+
+        this.setState({ nome: viewClick.title });
+        this.setState({ img: viewClick.thumbnail.path + "." + viewClick.thumbnail.extension });
+        // this.setState({ autores: viewClick.creators.items[0].name });
+    }
+
+
     render() {
         const { comics } = this.state;
         return (
-            <div id="container_comics" >
-                {comics.map(comic => (
-                    <div className="item_comic" key={comic.id}>
-                        <img src={comic.thumbnail.path + "." + comic.thumbnail.extension} />
-                        <h2 className="name_comic">{comic.title}</h2>
-                    </div>
-                ))}
+            <div>
+                <div className="painel">
+                    <p>Nome : {this.state.nome}</p>
+                    <p>Descricao : {this.state.descricao}</p>
+                    <img src={this.state.img}></img>
+                    <p>Autores : {this.state.autores}</p>
+                </div>
+
+                <div id="container_comics">
+                    {comics.map(comic => (
+                        <div className="card_comic" key={comic.id} onClick={() => this.openComic(comic.id)}>
+                            <div className="card_comic_item">
+                                <div className="card_comic_item_img" style={{ backgroundImage: 'url(' + comic.thumbnail.path + "." + comic.thumbnail.extension + ')' }} />
+                                <h2 className="card_comic_item_title">{comic.title}</h2>
+                            </div>
+                        </div>
+                    ))}
+                </div >
             </div >
         )
     }
